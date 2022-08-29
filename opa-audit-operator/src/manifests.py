adcbfe53c9003efa08da2d3a75b627b3474f8846
Nikos <nikos.sklikas@canonical.com>
2022-08-29 18:42:53 +0300

    Update manifest files

--- opa-audit-operator/src/manifests.py
+++ opa-audit-operator/src/manifests.py
@@ -56,9 +56,20 @@ mutating_webhook = from_dict(
     )
 )
 
-pod_disruption_budget = from_dict(
+pod_disruption_budget_beta = from_dict(
     dict(
         apiVersion="policy/v1beta1",
+        kind="PodDisruptionBudget",
+        metadata=dict(
+            name="gatekeeper-controller-manager", namespace="PodDisruptionBudget"
+        ),
+    )
+)
+
+
+pod_disruption_budget = from_dict(
+    dict(
+        apiVersion="policy/v1",
         kind="PodDisruptionBudget",
         metadata=dict(
             name="gatekeeper-controller-manager", namespace="PodDisruptionBudget"
@@ -101,6 +112,7 @@ class ControllerManagerManifests(Manifests):
             SubtractEq(self, validating_webhook),
             SubtractEq(self, mutating_webhook),
             SubtractEq(self, pod_disruption_budget),
+            SubtractEq(self, pod_disruption_budget_beta),
             ManifestLabel(self),
             ModelNamespace(self),
             RoleBinding(self),
